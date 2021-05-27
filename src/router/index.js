b1e3226edc2a6a9bed4f23937be64e9dcf56b0b3
qinxiaojun <315987083@qq.com>
2021-05-27 18:32:49 +0800

    Add files via upload

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+/*
+ * @Descripttion: 
+ * @Date: 2021-05-27 18:01:14
+ */
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router(
+  {
+    
+  }
+)
